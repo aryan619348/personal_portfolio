830bec1126a327ec165240ba702307a4f1709975
aryan619348 <aryan619348@gmail.com>
2023-07-18 16:34:22 +0530

    added expereinces

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify,request
 
 app = Flask(__name__)
 
@@ -14,10 +14,28 @@ def chatbot():
     # Add your chatbot logic here
     return render_template('chat.html')
 
+@app.route('/api/chat', methods=['POST'])
+def chat():
+    data = request.get_json()
+    message = data['message']
+
+    # Your chatbot logic goes here
+    # Process the user's message and generate a system reply
+
+    # For example, let's say the system reply is stored in the 'system_reply' variable
+    system_reply = "This is the system's reply."
+
+    response = {
+        'reply': system_reply
+    }
+
+    return jsonify(response)
+
 @app.route('/experiences')
 def experiences():
     # Add your logic to fetch and pass past experiences data to the template
     return render_template('experiences.html')
+
 
 @app.route('/projects')
 def projects():
@@ -33,6 +51,8 @@ def skills():
 def certifications():
     # Add your logic to fetch and pass certifications data to the template
     return render_template('certifications.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True,port=8000)
